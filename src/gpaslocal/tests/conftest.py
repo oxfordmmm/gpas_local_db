99ed4994ed837c499fe6b9ff4a274364171b3032
marc-brouard <marc.brouard@ndm.ox.ac.uk>
2024-03-03 14:01:50 +0000

    feat: Start adding unit tests

--- src/gpaslocal/tests/conftest.py
+++ src/gpaslocal/tests/conftest.py
@@ -1,22 +1,75 @@
 # conftest.py
 import pytest
+import logging
+from datetime import date
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from gpaslocal.db import Model
 from gpaslocal import models  # noqa: F401
 from sqlalchemy.pool import NullPool
 
-
 @pytest.fixture(scope="function")
-def test_session(postgresql):
+def db_session(postgresql) -> scoped_session:
     """Fixture to create a database session for testing."""
     # Create a SQLAlchemy engine using the URL provided by pytest-postgresql
     connection = f"postgresql+psycopg2://{postgresql.info.user}:@{postgresql.info.host}:{postgresql.info.port}/{postgresql.info.dbname}"
-    print(connection)
     engine = create_engine(connection, echo=False, poolclass=NullPool)
     Model.metadata.create_all(engine)
     Session = scoped_session(sessionmaker(bind=engine))
     session = Session()
+    
+    ## Add owners
+    session.add(models.Owner(site="SiteC", user="User3"))
+    session.add(models.Owner(site="blah", user="blah-user"))
+    
+    ## add countries
+    session.add(models.Country(code="GBR", name="United Kingdom", code2="UK", lat=54.0, lon=-2.0))
+    
+    ## add specimen_detail_types, omitting description as not really needed for testing
+    specimen_detail_types = [
+        {"code": "organism", "description": "", "value_type": "str"},
+        {"code": "host", "description": "", "value_type": "str"},
+        {"code": "host_diseases", "description": "", "value_type": "str"},
+        {"code": "isolation_source", "description": "", "value_type": "int"},
+        {"code": "lat", "description": "", "value_type": "float"},
+        {"code": "lon", "description": "", "value_type": "float"},
+    ]
+    for detail_type in specimen_detail_types:
+        session.add(models.SpecimenDetailType(**detail_type))
+    
+    ## add sample_detail_types
+    sample_detail_types = [
+        {"code": "extraction_method", "description": "", "value_type": "str"},
+        {"code": "extraction_protocol", "description": "", "value_type": "float"},
+        {"code": "extraction_date", "description": "", "value_type": "date"},
+        {"code": "extraction_user", "description": "", "value_type": "str"},
+        {"code": "dna_amplification", "description": "", "value_type": "bool"},
+        {"code": "pre_sequence_concentration", "description": "", "value_type": "float"},
+        {"code": "dilution_post_initial_concentration", "description": "", "value_type": "bool"},
+        {"code": "input_volume", "description": "", "value_type": "float"},
+    ]
+    for detail_type in sample_detail_types:
+        session.add(models.SampleDetailType(**detail_type))
+    
+    ## add runs
+    session.add(models.Run(code="Run1", site="SiteA", sequencing_method="Illumina", machine="Machine1"))
+    session.add(models.Run(code="test1", run_date=date.fromisoformat("2024-01-01"), site="Oxford", sequencing_method="Illumina", machine="test_m1", user="blah", number_samples=2, flowcell="fc2", passed_qc=True, comment="test_comment"))
+    
+    ## add specimens
+    session.add(models.Specimen(
+        accession="123test",
+        collection_date=date.fromisoformat("2021-01-01"),
+        country_sample_taken_code="GBR",
+        owner=session.query(models.Owner).first(),
+    ))
+    
+    # commit the added records
+    session.commit()
+    
     # Here you can also set up your database schema if needed
     yield session  # Provide the session for the test
     session.close()  # Clean up after the test
+    
+@pytest.fixture(autouse=True)
+def set_caplog_level(caplog):
+    caplog.set_level(logging.INFO, logger='gpas-local')
--- src/gpaslocal/tests/test_importer.py
+++ src/gpaslocal/tests/test_importer.py
@@ -1,7 +1,130 @@
+import pytest
+import os
+from datetime import date
 from sqlalchemy import text
+from gpaslocal.upload_models import SpecimensImport
+from gpaslocal import models
+from gpaslocal.importer import owner, find_run, find_specimen, runs
 
 
-def test_some_database_interaction(test_session):
+def test_some_database_interaction(db_session):
     # Use test_session directly here to interact with the database
-    result = test_session.execute(text("SELECT * FROM owners"))
+    result = db_session.execute(text("SELECT * FROM owners"))
     assert result.rowcount >= 0
+
+@pytest.mark.parametrize(
+    "index, owner_site, owner_user, dryrun, expected_log, test_id",
+    [
+        (
+            0,
+            "SiteA",
+            "User1",
+            False,
+            "Specimens Sheet Row 2: Owner SiteA, User1 does not exist, adding",
+            "happy_path_new_owner",
+        ),
+        (
+            1,
+            "SiteB",
+            "User2",
+            True,
+            "Specimens Sheet Row 3: Owner SiteB, User2 does not exist",
+            "happy_path_dryrun",
+        ),
+        (2, "SiteC", "User3", False, None, "happy_path_existing_owner"),
+    ],
+)
+def test_owner(
+    db_session,
+    index,
+    owner_site,
+    owner_user,
+    dryrun,
+    expected_log,
+    test_id,
+    caplog,
+):
+    # Arrange
+    specimen_import = SpecimensImport(
+        owner_site=owner_site,
+        owner_user=owner_user,
+        accession="123test",
+        collection_date="2021-01-01",
+        country_sample_taken_code="GBR",
+    )
+
+    # Act
+    result = owner(db_session, index, specimen_import, dryrun)
+
+    # Assert
+    assert isinstance(result, models.Owner)
+    assert result.site == owner_site
+    assert result.user == owner_user
+    if expected_log:
+        assert expected_log in caplog.text
+    else:
+        assert caplog.text == ""
+
+def test_find_run(db_session):
+    # Arrange
+    run_code = "Run1"
+    run_site = "SiteA"
+    run_sequencing_method = "Illumina"
+    run_machine = "Machine1"
+
+    # Act
+    result = find_run(db_session, run_code)
+
+    # Assert
+    assert isinstance(result, models.Run)
+    assert result.code == run_code
+    assert result.site == run_site
+    assert result.sequencing_method == run_sequencing_method
+    assert result.machine == run_machine
+    assert result.id == 1
+    assert result.created_at
+    assert result.updated_at
+
+
+def test_find_run_not_found(db_session):
+    # Arrange
+    run_code = "Run2"
+
+    # Act and assert
+    with pytest.raises(ValueError):
+        find_run(db_session, run_code)
+
+def test_find_specimen(db_session):
+    # Arrange
+    specimen_accession = "123test"
+    specimen_collection_date = date.fromisoformat("2021-01-01")
+    specimen_country_sample_taken_code = "GBR"
+
+    result = find_specimen(db_session, specimen_accession, specimen_collection_date)
+
+    assert isinstance(result, models.Specimen)
+    assert result.accession == specimen_accession
+    assert result.collection_date == specimen_collection_date
+    assert result.country_sample_taken_code == specimen_country_sample_taken_code
+    assert result.id == 1
+    assert result.created_at
+    assert result.updated_at
+
+def test_find_specimen_not_found(db_session):
+    # Arrange
+    specimen_accession = "456test"
+    specimen_collection_date = date.fromisoformat("2021-01-01")
+    
+    with pytest.raises(ValueError):
+        find_specimen(db_session, specimen_accession, specimen_collection_date)
+
+def test_runs_upload_ok_new(db_session, caplog):
+    # Arrange
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    xl_ok = os.path.join(dir_path, 'data', 'test_no_errors.xlsm')
+    
+    runs(db_session, xl_ok, dryrun=True)
+    
+    assert "Sheet Row 2: Run test1 already exists" in caplog.text
+    assert "Runs Sheet Row 3: Run test2 does not exist" in caplog.text
+    
